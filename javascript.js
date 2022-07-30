5d3bf66750c401a4d6a8c57f7695d6e7c563967e
Parham Adiban <padiban94@gmail.com>
2022-07-29 20:41:40 -0400

    added grid, prompt, mouseevent

--- javascript.js
+++ javascript.js
@@ -1,11 +1,37 @@
-// buttons is a node list. It looks and acts much like an array.
-const buttons = document.querySelectorAll('button');
+const container = document.querySelector('#container')
+let gridsize = prompt("Enter grid size (maximum = 100)")
 
-// we use the .forEach method to iterate through each button
-buttons.forEach((button) => {
+if (gridsize > 100) {
+  gridsize = 100
+}
 
-  // and for each one we add a 'click' listener
-  button.addEventListener('click', () => {
-    alert(button.id);
-  });
-});
+for (let i = 0; i < gridsize; i++){
+
+  const rowdiv = document.createElement("div");
+  rowdiv.setAttribute('id', `${i+1}`);
+  rowdiv.classList.add('rows');
+  container.appendChild(rowdiv);
+
+  for (let j = 0; j < gridsize; j++){
+
+    const coldiv = document.createElement("div");
+    coldiv.setAttribute('id', `${j+1}`);
+    coldiv.textContent = `${j+1}`;
+    coldiv.classList.add('cols');
+    coldiv.addEventListener('mouseover', function(e) {
+      e.target.style.background = 'yellow';
+    });
+    rowdiv.appendChild(coldiv);
+
+  }
+}
+
+function resett(){
+  const cells = document.querySelectorAll('.cols');
+  
+}
+
+// const cells = document.querySelectorAll('.cols');
+// cells.addEventListener('hover', function(e) {
+//   e.target.style.background = 'yellow';
+// });
