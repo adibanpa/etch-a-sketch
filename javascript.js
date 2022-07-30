6a4e5d6805222d6d485a50e35a1f436bf9864a52
Parham Adiban <padiban94@gmail.com>
2022-07-29 22:33:17 -0400

    added new grid button, fixed size of the grid

--- javascript.js
+++ javascript.js
@@ -1,28 +1,28 @@
-const container = document.querySelector('#container')
-let gridsize = prompt("Enter grid size (maximum = 100)")
+function setGrid(gridsize=16) {
 
-if (gridsize > 100) {
-  gridsize = 100
-}
+  const container = document.querySelector('#container')
 
-for (let i = 0; i < gridsize; i++){
+  for (let i = 0; i < gridsize; i++){
 
-  const rowdiv = document.createElement("div");
-  rowdiv.setAttribute('id', `${i+1}`);
-  rowdiv.classList.add('rows');
-  container.appendChild(rowdiv);
+    const rowdiv = document.createElement("div");
+    rowdiv.setAttribute('id', `${i+1}`);
+    rowdiv.classList.add('rows');
+    container.appendChild(rowdiv);
 
-  for (let j = 0; j < gridsize; j++){
+    for (let j = 0; j < gridsize; j++){
 
-    const coldiv = document.createElement("div");
-    coldiv.setAttribute('id', `${j+1}`);
-    coldiv.classList.add('cols');
-    coldiv.addEventListener('mouseover', function(e) {
-      e.target.style.background = 'yellow';
-    });
-    rowdiv.appendChild(coldiv);
+      const coldiv = document.createElement("div");
+      coldiv.setAttribute('id', `${j+1}`);
+      coldiv.classList.add('cols');
+      // coldiv.textContent = 'H';
+      coldiv.addEventListener('mouseover', function(e) {
+        e.target.style.background = 'yellow';
+      });
+      rowdiv.appendChild(coldiv);
 
+    }
   }
+    
 }
 
 function resett(){
@@ -30,5 +30,16 @@ function resett(){
   cells.forEach((cell) => {
     cell.style.background = 'dodgerblue';
   })
-  
+}
+
+setGrid()
+
+function clickme(){
+  document.querySelector('#container').innerHTML="";
+
+  let grdsize = prompt("Enter grid size (Maximum of 100)");
+  if (grdsize > 100 ) {
+    grdsize = 100;
+  }
+  setGrid(grdsize);
 }
